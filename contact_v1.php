a22632d4b39a796d559ed05d227e29cf9cec76bd
3GabrielGarcia <3ggarcia@tawacollege.school.nz>
2026-09-14 02:52:13 +1200

    added stage 3 v1 files

--- contact_v1.php
+++ contact_v1.php
@@ -2,7 +2,7 @@
 <html>
    <head>
     <title>IDIDIKIT Website</title><!-- Metadata and title -->
-        <link rel="icon" type="image/x-icon" href="" >
+        <link rel="icon" type="image/x-icon" href="images\logo.png" >
             <meta charset="utf-8">	
             <meta name="Keyword" content="Ididikit, NZ, Custom Design, Request, laser engraving, Vinyl Printing, Commission"/>
             <meta name="Author" content="Gabriel Zeaph Garcia"/>
@@ -20,7 +20,7 @@
                     <ul>
                         <li><a href="index_v1.html" class="navtext">Home</a></li>
                         <li><a href="page1_v1.html" class="navtext">Product Gallery</a></li>
-                        <li><div class="activepage"><a href="contact_v1.php" class="navtext">Request A Design</a></div></li>
+                        <li><div class="activepage"><a href="contact_v1.php" class="navtext"	>Request A Design</a></div></li>
                     </ul>	
                 </div> 
             </div>
@@ -45,13 +45,14 @@
             <div id="logo"><img src="images\ididikitLogoHorizontal.png" alt="Ididikit logo" id="idlogo"></div>
             </div> <!--nav div end -->
 
-            <div class="header">
-                <!-- <img src="images\404NotCreated.jpg" alt="Image of an IDIDIKIT Product" id="headerimage"> -->
-                <div id="headertextbox">
-                    <h1 id="headertext">Request A <br> Design</h1>
+            <div class="header" id="contactheader">
+                <!-- <div id="headerimage"><img src="images/NotebookPen.png" alt="Ididikit Logo" id="headerimage"></div> -->
+                <div id="backgroundimgage">
+                    <div id="headertextbox">
+                        <h1 id="headertext">Request A Design</h1>
+                    </div>
                 </div>
             </div> <!-- header div end -->
-            <script type="text/javascript" src="js/myscript_v1.js"></script>	
             <div class="content">
                 <hr>
 
@@ -62,7 +63,7 @@
                     ?>
                         
             
-                    <div class="contact_container">
+                    <div class="contactcontainer">
                         <h1>Request A Design</h1>
                         <form action="connect.php" method="post">
                             <div class="row">
@@ -112,23 +113,29 @@
 
                                 <div class="column2">
                                 
-                                <textarea id="message" name="message" placeholder="Write Something..." style="height: 200px;"></textarea>
+                                <textarea id="message" name="message" placeholder="Write Something..."></textarea>
                                 
                                 </div>
                             </div>
-                            <div id="submitbutton">
-                                <div class="row">
-                                    <input type="submit" value="submit">
+                            
+                                <div class="row" id="formbutton">
+                                    <input id="submitbutton" type="submit" value="submit">
                                     
                                 </div>
-                            </div>
                         </form>
-
-                        
                     </div>
+                    <br>
+                    <div class="contactcontainer">
+                        <h1 class="infoheader">Contact Us</h1>
+                        <p>Email: Ididikit.nz@gmail.com</p>
+                        <p>Phone: 220329826</p>
+                        <p>Facebook: Ididikit</p>
+                        <p>Instragram: @ididikit.nz</p>
+                    </div>
+                    
 
                 </div>  
-            </div>  
+            </div>  <!--Content div end-->
             <div class="footer"><!-- this is the footer -->
 				<div class="footerelementbox">
 					<div id="footerimg">
@@ -142,17 +149,47 @@
 
 					<div class="footerflex">
 						<div class="footerlinkflex">
-							<a><h1 class="footerlink">Instagram</h1></a>
+							<div><img src="images\linkicon.png" alt="Link Icon" id="linkicon"></div>
+							<a href="https://www.instagram.com/ididikit.nz/"><h1 class="footerlink">Instagram</h1></a>
 						</div>
 
 						<div class="footerlinkflex">	
-							<a><h1 class="footerlink">Facebook</h1></a>
+							<div><img src="images\linkicon.png" alt="Link Icon" id="linkicon"></div>
+							<a href="https://www.facebook.com/ididikit.nz/"><h1 class="footerlink">Facebook</h1></a>
 						</div>
+					</div>
+				</div>
+
+                <div class="footerelementboxresponsive">
+					<div>
+					<div id="footerimgresponsive">
+						<div><img src="images\ididikit-01.png" alt="Ididikit Long" id="idimagefooter"></div>
+						<div><h1 class="footertextheader">&copy; Ididikit 2026, All rights reserved</h1></div>
+					</div>
+
+					<div>
+						<p class="footertexttop">Other Platforms</p>
+					</div>
+
+					<div class="footerflexresponsive">
+						<div class="footerlinkflex">
+							<div><img src="images\linkicon.png" alt="Link Icon" id="linkicon"></div>
+							<a href="https://www.instagram.com/ididikit.nz/"><h1 class="footerlink">Instagram</h1></a>
+						</div>
+
+						<div class="footerlinkflex">
+							<div><img src="images\linkicon.png" alt="Link Icon" id="linkicon"></div>	
+							<a href="https://www.facebook.com/ididikit.nz/"><h1 class="footerlink">Facebook</h1></a>
+						</div>
+					</div>
+
+					<div id="footertextflex">
+						<p class="footertext">Created By Gabriel Garcia</p>
 					</div>
 				</div>
 		</div>
     </div>
     
     </body>
-
+    <script type="text/javascript" src="js/myscript_v1.js"></script>	
 </html>
